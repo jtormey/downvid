cee8cd6a802356c82282c2562f6e3a5f8e3ca952
Justin Tormey <jrtormey@gmail.com>
2018-04-15 20:57:53 -0400

    tmp: getting fs permissions

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,28 @@
 import React from 'react'
 
 class App extends React.Component {
+  requestFs = () => {
+    window.webkitRequestFileSystem(
+      window.PERSISTENT,
+      1024 * 1024 /* size bytes */,
+      this.onFileSystemInit,
+      this.onFileSystemError
+    )
+  }
+
+  onFileSystemInit = (fs) => {
+    console.log('Success!', fs)
+  }
+
+  onFileSystemError = (error) => {
+    console.log('Error:', error)
+  }
+
   render () {
     return (
       <div>
         App
+        <button onClick={this.requestFs}>request fs</button>
       </div>
     )
   }
