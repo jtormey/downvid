584f0547f895259ab23035a7ef91a552cd3eb997
Justin Tormey <jrtormey@gmail.com>
2018-04-27 18:27:22 -0400

    prove idea conceptually

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 const express = require('express')
-// const ytdl = require('ytdl-core')
+const ytdl = require('ytdl-core')
 
+const PORT = 8081
 const app = express()
 
-app.get('/download', () => {
-  // implement download
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Method', '*')
+  res.header('Access-Control-Allow-Header', '*')
+
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+  } else {
+    next()
+  }
+})
+
+app.get('/download', (req, res) => {
+  let url = 'https://www.youtube.com/watch?v=JbepN4dKLbU'
+  ytdl(url).pipe(res)
+})
+
+app.listen(PORT, () => {
+  console.log(`downvid server listening on ${PORT}`)
 })
