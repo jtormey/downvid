cfb05093bfe1e35fa9560e0550ee1f947a211637
Justin Tormey <jrtormey@gmail.com>
2018-04-29 18:11:54 -0400

    add actual deploy process

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,11 @@
-echo "deployed"
+echo "\n>>> updating node modules\n"
+npm install
+
+echo "\n>>> building client application\n"
+rm -rf ./build
+npm run build
+
+echo "\n>>> restarting server\n"
+npm run pm2.restart
+
+echo "\n>>> deployed\n"
