00258d439d97188d179955ec3f93f1b27950e63f
Justin Tormey <jrtormey@gmail.com>
2018-05-10 17:07:21 -0400

    move condition for caching yt images

--- src/app/worker/service-worker.static.js
+++ src/app/worker/service-worker.static.js
@@ -3,8 +3,7 @@ const log = (...args) => console.log('[ServiceWorker]:', ...args)
 
 const cacheUrls = [
   '/',
-  '/main.build.js',
-  '/service-worker.static.js'
+  '/main.build.js'
 ]
 
 self.addEventListener('install', (event) => {
@@ -20,28 +19,26 @@ self.addEventListener('install', (event) => {
 self.addEventListener('fetch', (event) => {
   log(`Fetch event for url "${event.request.url}"`)
   event.respondWith(
-    (/i\.ytimg\.com/).test(event.request.url) ? (
-      caches.match(event.request).then((response) => {
-        if (response) {
-          log(`Matched response for "${event.request.url}"`)
+    caches.match(event.request).then((response) => {
+      if (response) {
+        log(`Matched response for "${event.request.url}"`)
+        return response
+      }
+
+      return fetch(event.request.clone()).then((response) => {
+        if (!response || response.status !== 200 || response.type !== 'basic') {
           return response
         }
 
-        return fetch(event.request.clone()).then((response) => {
-          if (!response || response.status !== 200 || response.type !== 'basic') {
-            return response
-          }
-
+        if (/i\.ytimg\.com/.test(event.request.url)) {
           caches.open(CACHE_NAME).then((cache) => {
             log(`Wrote response to cache "${CACHE_NAME}" for "${event.request.url}"`)
             cache.put(event.request, response.clone())
           })
+        }
 
-          return response
-        })
+        return response
       })
-    ) : (
-      fetch(event.request)
-    )
+    })
   )
 })
